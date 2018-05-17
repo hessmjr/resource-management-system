420c2aa809e069f798a6f2f5490e73c92d8332df
Mark <mark.hess2@gmail.com>
2018-05-17 12:13:12 -0500

    trying stuff

--- src/test.py
+++ src/test.py
@@ -12,9 +12,14 @@ def test_answer():
     assert inc(3) == 4
     assert another() == 'what'
 
+def test_reqest():
+    r = requests.get('http://httpbin.org/')
+    assert r.status_code == 200
+
 def test_ui():
     r = requests.get('http://localhost:5000/')
     assert r.status_code == 200
 
 test_answer()
+test_reqest()
 test_ui()
