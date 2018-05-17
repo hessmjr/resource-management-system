c866f2f96ce854710841593ca2bbe02c752e1c79
Mark <mark.hess2@gmail.com>
2018-05-17 11:57:56 -0500

    trying docker compose in travis

--- src/test.py
+++ src/test.py
@@ -1,4 +1,6 @@
-# content of test_sample.py
+import requests
+
+
 def inc(x):
     return x + 1
 
@@ -10,4 +12,9 @@ def test_answer():
     assert inc(3) == 4
     assert another() == 'what'
 
+def test_ui():
+    r = requests.get('http://localhost:5000/')
+    assert r.status_code == 200
+
 test_answer()
+test_ui()
