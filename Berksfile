181e0024017eafacf505e6aa8565c5c0ae1d187e
Mark <mark.hess2@gmail.com>
2019-02-20 13:11:11 -0600

    remove vagrant files

--- Berksfile
+++
@@ -1,6 +0,0 @@
-source "https://supermarket.chef.io"
-
-cookbook 'chef', path: './chef'
-cookbook 'mysql', '~> 7.1.0'
-cookbook 'poise-python'
-cookbook "supervisor", "~> 0.4.12"
--- chef/metadata.rb
+++
@@ -1,6 +0,0 @@
-name             'chef'
-description      'Chef cookbook recipe for deploying the ERMS application'
-
-depends          'mysql'
-depends          'poise-python'
-depends          'supervisor'
